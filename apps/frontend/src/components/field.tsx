a5865b735fadf4c601b3371ca6c42b09a36bc92b
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2025-05-10 19:24:30 -0300

    add validation messages to field component

--- apps/frontend/src/components/field.tsx
+++ apps/frontend/src/components/field.tsx
@@ -1,19 +1,23 @@
+import clsx from "clsx";
 import { PropsWithChildren } from "react";
 
 type Props = Readonly<PropsWithChildren<{
-  error?: string
+  errors?: string[]
   required?: boolean
   id: string
   label: string
 }>>
 
-export function Field({ children, id, label, required }: Props) {
+export function Field({ children, id, label, required, errors = [] }: Props) {
   return (
     <fieldset className="flex flex-col gap-1">
       <label htmlFor={id}>{label} {required && <span className="text-red-400">*</span>}</label>
-      <div className="border border-foreground rounded-md p-2">
+      <div className={clsx("border rounded-md p-2", { "border-red-500 text-red-500": errors.length, "border-foreground": !errors.length })}>
         {children}
       </div>
+      {errors.map((message, index) => (
+        <span key={index} className="text-red-500">{message}</span>
+      ))}
     </fieldset>
   )
 }
