a73b7c865765c8eb3ddb2f0783377501a77b937b
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2025-05-13 11:00:11 -0300

    add user table indexes

--- apps/backend/sql/migrations/0001_user_table_create.down.sql
+++ apps/backend/sql/migrations/0001_user_table_create.down.sql
@@ -1,3 +1,5 @@
+DROP INDEX users_email_unique;
+
 drop table if exists users cascade;
 drop type user_role;
 
--- apps/backend/sql/migrations/0001_user_table_create.up.sql
+++ apps/backend/sql/migrations/0001_user_table_create.up.sql
@@ -1,3 +1,4 @@
+
 CREATE TYPE "user_role" AS ENUM (
   'regular_user',
   'admin_user'
@@ -9,6 +10,10 @@ CREATE TABLE "users" (
   "email" varchar NOT NULL,
   "role" user_role NOT NULL,
   "password" varchar,
-  "created_at" timestamp NOT NULL DEFAULT (now())
+  "created_at" timestamp NOT NULL DEFAULT (now()),
+  "deleted_at" timestamp
 );
 
+CREATE INDEX ON "users" ("email");
+
+CREATE UNIQUE INDEX users_email_unique ON "users" USING btree ("email", "deleted_at");
