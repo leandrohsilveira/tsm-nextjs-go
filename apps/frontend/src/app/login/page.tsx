48027072572e14ac1ec6b2f994986516870d23ea
Leandro Hinckel Silveira <leandro.hinckel@gmail.com>
2025-05-13 14:45:27 -0300

    add login result handling

--- apps/frontend/src/app/login/page.tsx
+++ apps/frontend/src/app/login/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Field, PageLayout } from "@/components";
+import { Field, Message, PageLayout } from "@/components";
 import { loginAction } from "@/lib/auth";
 import { useActionState } from "react";
 
@@ -12,17 +12,20 @@ export default function Login() {
   )
 
   return (
-    <PageLayout>
-      <h3 className="font-bold text-2xl text-center w-full">Sign-in</h3>
-      <form className="w-full flex flex-col gap-4" action={action}>
-        <Field id="username" label="E-mail" errors={state.errors.username} required>
-          <input type="text" id="username" name="username" placeholder="Enter e-mail" inputMode="email" autoComplete="username" disabled={pending} />
-        </Field>
-        <Field id="password" label="Password" errors={state.errors.password} required>
-          <input type="password" id="password" name="password" placeholder="Enter password" autoComplete="current-password" disabled={pending} />
-        </Field>
-        <button type="submit" disabled={pending}>Login</button>
-      </form>
-    </PageLayout>
+    <>
+      <Message kind="error" text={state.message} />
+      <PageLayout>
+        <h3 className="font-bold text-2xl text-center w-full">Sign-in</h3>
+        <form className="w-full flex flex-col gap-4" action={action}>
+          <Field id="username" label="E-mail" errors={state.errors.username}>
+            <input type="email" id="username" name="username" placeholder="Enter e-mail" autoComplete="username" disabled={pending} required />
+          </Field>
+          <Field id="password" label="Password" errors={state.errors.password}>
+            <input type="password" id="password" name="password" placeholder="Enter password" autoComplete="current-password" disabled={pending} required />
+          </Field>
+          <button type="submit" disabled={pending}>Login</button>
+        </form>
+      </PageLayout>
+    </>
   )
 }
